48312b53c8dea98fe8e195e8880c337396e801fa
Thando Nkala <nkalathando01@gmail.com>
2025-10-10 13:32:18 +0200

    mmm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/Comps/Navbar";
-import Footer from "@/Comps/Footer";
+import Navbar from "../Comps/Navbar";
+import Footer from "../Comps/Footer";
 import { UserProvider } from "../app/context/UserContext"; // adjust path if need
 
 
