48ecd3f2903fabc3c4f2cd7538850f5fb6270289
Thando Nkala <nkalathando01@gmail.com>
2025-10-16 11:17:11 +0200

    thatoo

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
-
-module.exports = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
-        
       },
     ],
   },
-}
+};
+
+export default nextConfig;
